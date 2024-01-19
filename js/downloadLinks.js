e24a1e302c2a106056f718c95e48577c4f264699
Realitylion <dhanushbaalaji123@gmail.com>
2024-01-19 15:55:10 +0530

    fix: downloadLink format

--- js/downloadLinks.js
+++ js/downloadLinks.js
@@ -6,9 +6,9 @@ const windowsAnchorTag = document.getElementById("windows-download-link");
 const linuxAnchorTag = document.getElementById("linux-download-link");
 const macAnchorTag = document.getElementById("mac-download-link");
 
-windowsAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/buildFiles/v${data.version.windows}/raw/main/vtop-cli_windows_v${data.version.windows}.exe`;
-linuxAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/buildFiles/v${data.version.linux}/raw/main/vtop-cli_linux_v${data.version.linux}.exe`;
-macAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/buildFiles/v${data.version.mac}/raw/main/vtop-cli_mac_v${data.version.mac}.exe`;
+windowsAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/raw/main/buildFiles/v${data.version.windows}/vtop-cli_windows_v${data.version.windows}.exe`;
+linuxAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/raw/main/buildFiles/v${data.version.linux}/vtop-cli_linux_v${data.version.linux}.exe`;
+macAnchorTag.href = `https://github.com/Realitylion/vtop-cli-website/raw/main/buildFiles/v${data.version.mac}/vtop-cli_mac_v${data.version.mac}.exe`;
 
 windowsAnchorTag.innerText = `v${data.version.windows}`;
 linuxAnchorTag.innerText = `v${data.version.linux}`;
